78fc800f3deb30b29304387b9754c9339df3871c
Accel <denisignatenko2006@gmail.com>
2026-01-20 21:06:30 +0700

    Добавьте файлы проекта.

---
+++ SmallManufacturing/Pages/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmallManufacturing.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMaterials_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnProducts_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LVOrder_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
